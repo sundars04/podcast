625d729bee17d22e1986e6b3f4bc5d18b4334637
Sundars0 <Sundars04@outlook.com>
2016-12-05 15:16:18 +0000

    add podcast controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :podcasts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  resources :podcasts, only: [:index, :show]
 end
