db73ca2deaf7472332b738d5321b08a206f9daa0
Sundars0 <Sundars04@outlook.com>
2016-12-05 15:40:41 +0000

    add episode controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :podcasts, only: [:index, :show]
+  resources :podcasts, only: [:index, :show] do
+    resources :episodes
+  end
 end
