c83c5b143dde1b838abb7a2673ec219245b9a1df
Sundars0 <Sundars04@outlook.com>
2016-12-05 14:12:26 +0000

    add attributes to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205133147) do
+ActiveRecord::Schema.define(version: 20161205135303) do
 
   create_table "podcasts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20161205133147) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "itunes"
+    t.string   "stitcher"
+    t.string   "podbay"
     t.index ["email"], name: "index_podcasts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_podcasts_on_reset_password_token", unique: true
   end
