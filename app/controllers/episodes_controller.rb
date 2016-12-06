aa09198cefb84b306481fed09eeb1013fe01ba13
Sundars0 <Sundars04@outlook.com>
2016-12-06 04:58:15 +0000

    modify controller

---
+++ app/controllers/episodes_controller.rb
@@ -0,0 +1,51 @@
+class EpisodesController < ApplicationController
+  before_action :find_podcast
+  before_action :find_episode, only: [:show, :edit, :update, :destroy]
+
+  def show
+    @episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id == @episode.id }
+  end
+
+  def new
+    @episode = @podcast.episodes.new
+  end
+
+  def create
+    @episode = @podcast.episodes.new(episode_params)
+    if @episode.save
+      redirect_to podcast_episode_path(@podcast, @episode)
+    else
+      render 'new'
+    end
+  end
+
+  def edit    
+  end
+
+  def update
+    if @episode.update(episode_params)
+      redirect_to podcast_episode_path(@podcast, @episode), notice: "Episode was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @episode.destroy
+    redirect_to root_path, notice: "Episode was successfully Deleted!"
+  end
+
+  private
+
+    def find_podcast
+      @podcast = Podcast.find(params[:podcast_id])
+    end
+
+    def find_episode
+      @episode = Episode.find(params[:id])      
+    end
+
+    def episode_params
+      params.require(:episode).permit(:title, :descripton)
+    end
+end
